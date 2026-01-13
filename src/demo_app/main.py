8ab33463500d1a94fedb9034ad7dbcf276ec703a
unknown <doruk@student.agh.edu.pl>
2026-01-13 04:36:42 +0300

    asdasdasd

--- src/demo_app/main.py
+++ src/demo_app/main.py
@@ -1,11 +1,12 @@
 import inspect
 import pyspark.sql.udtf as u
 
-print("Python OK")
-print("UDTF file:", inspect.getsourcefile(u))
-
-print("AnalyzeArgument exists:", hasattr(u, "AnalyzeArgument"))
+print("Python:", __import__("sys").version)
+print("UDTF file:", u.__file__)
+print("AnalyzeArgument seists:", hasattr(u, "AnalyzeArgument"))
 
 if hasattr(u, "AnalyzeArgument"):
     print("Defined in:", inspect.getsourcefile(u.AnalyzeArgument))
     print(inspect.getsource(u.AnalyzeArgument))
+else:
+    print("AnalyzeArgument NOT present")
